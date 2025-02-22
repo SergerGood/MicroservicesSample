59d596e39a4a7d05311049e408dff7a9bd2d3af2
sergergood <serzher@inbox.ru>
2025-02-22 21:45:30 +0300

    Set up Carter and MediatR

--- src/Services/Catalog/Program.cs
+++ src/Services/Catalog/Program.cs
@@ -1,4 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(configuration =>
+{
+    configuration.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
