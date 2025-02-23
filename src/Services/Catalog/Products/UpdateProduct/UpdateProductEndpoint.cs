e3127e9b877b1dd87d279801ff7439016dfe21e7
sergergood <serzher@inbox.ru>
2025-02-23 21:07:05 +0300

    Add UpdateProduct endpoint and command handler

---
+++ src/Services/Catalog/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,34 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest(
+    Guid Id,
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+
+public record UpdateProductResponse(bool IsSuccess);
+
+public class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", Handle)
+            .WithName("UpdateProduct")
+            .Produces<UpdateProductResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Updates a product")
+            .WithDescription("Updates a product in the catalog");
+    }
+
+    private static async Task<IResult> Handle(UpdateProductRequest request, ISender sender)
+    {
+        var command = request.Adapt<UpdateProductCommand>();
+        var result = await sender.Send(command);
+        var response = result.Adapt<UpdateProductResponse>();
+
+        return Results.Ok(response);
+    }
+}
