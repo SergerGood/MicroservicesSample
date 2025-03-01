fe71a43d47482aacb713ee4f9f2bb771fb600c77
sergergood <serzher@inbox.ru>
2025-03-01 12:10:00 +0300

    Add DeleteBasket endpoints and handler

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -1,6 +1,27 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketEndpoints
+public record DeleteBasketRequest(string UserName);
+
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoints : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}", Handle)
+            .WithName("DeleteProduct")
+            .Produces<DeleteBasketResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+    }
+
+    private async Task<IResult> Handle(string userName, ISender sender)
+    {
+        var result = await sender.Send(new DeleteBasketCommand(userName));
+        var response = result.Adapt<DeleteBasketResult>();
+
+        return Results.Ok(response);
+    }
 }
