f81d5c1e603f7953e5e6445980c16ea0efbd14fa
sergergood <serzher@inbox.ru>
2025-03-01 11:43:23 +0300

    Add models and endpoints for shopping cart functionality

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketRequest(string UserName);
+
+public record GetBasketResponse(ShoppingCart Cart);
+
+public class GetBasketEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}", Handle)
+            .WithName("GetBasket")
+            .Produces<GetBasketResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Basket")
+            .WithDescription("Get Basket");
+    }
+
+    private static async Task<IResult> Handle(string userName, ISender sender)
+    {
+        var request = new GetBasketRequest(userName);
+        var result = await sender.Send(request);
+        var response = result.Adapt<GetBasketResponse>();
+
+        return Results.Ok(response);
+    }
+}
---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public ShoppingCart()
+    {
+    }
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    public string UserName { get; set; }
+    public List<ShoppingCartItem> Items { get; set; } = [];
+    
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
