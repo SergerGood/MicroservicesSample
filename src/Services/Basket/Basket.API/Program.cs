4f3f15487cdd1602f8818e10aefc55508263e320
sergergood <serzher@inbox.ru>
2025-03-02 21:09:28 +0300

    Add health checks

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,8 @@
 using Basket.API.Data;
 using BuildingBlocks.Behaviors;
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,28 +23,41 @@ builder.Services
     .AddScoped<IBasketRepository, BasketRepository>()
     .AddMarten(options =>
     {
-        options.Connection(GetConnectionString(builder));
+        options.Connection(GetDbConnectionString(builder));
         options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
     })
     .UseLightweightSessions();
 
 builder.Services
     .Decorate<IBasketRepository, CachedBasketRepository>()
-    .AddStackExchangeRedisCache(options =>
-    {
-        options.Configuration = builder.Configuration.GetConnectionString("Redis");
-    });
+    .AddStackExchangeRedisCache(options => options.Configuration = GetRedisConnectionString(builder));
+
+builder.Services
+    .AddHealthChecks()
+    .AddNpgSql(GetDbConnectionString(builder))
+    .AddRedis(GetRedisConnectionString(builder));
 
 var app = builder.Build();
 
 app.MapCarter();
 app.UseExceptionHandler(_ => { });
+app.UseHealthChecks("/hc",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
 return;
 
-string GetConnectionString(WebApplicationBuilder webApplicationBuilder)
+string GetDbConnectionString(WebApplicationBuilder webApplicationBuilder)
 {
     return webApplicationBuilder.Configuration.GetConnectionString("Database")
            ?? throw new InvalidOperationException("Database connection string is missing");
+}
+
+string GetRedisConnectionString(WebApplicationBuilder webApplicationBuilder)
+{
+    return webApplicationBuilder.Configuration.GetConnectionString("Redis")
+           ?? throw new InvalidOperationException("Redis connection string is missing");
 }
