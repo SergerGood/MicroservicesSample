90a152a22451b7dd1424ed98fbdd2750f11cc2a0
sergergood <serzher@inbox.ru>
2025-03-04 22:45:54 +0300

    Add Entity Framework Core support for Discount service with SQLite

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,18 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
+{
+    public DbSet<Coupon> Coupons { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>()
+            .HasData(
+                new Coupon { Id = 1, ProductName = "IPhone", Description = "IPhone", Amount = 1 },
+                new Coupon { Id = 2, ProductName = "Honor", Description = "Honor", Amount = 2 }
+            );
+    }
+}
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,10 +1,17 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(optionsBuilder =>
+    optionsBuilder.UseSqlite(configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
+
+app.UseMigration();
 
 app.MapGrpcService<DiscountService>();
 app.MapGet("/",
