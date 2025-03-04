347e7ac61b8db24be06116267047fc90b2c1acd9
sergergood <serzher@inbox.ru>
2025-03-04 23:05:50 +0300

    Enhance Discount service to retrieve discounts from database

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,12 +1,25 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services;
 
-public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        return base.GetDiscount(request, context);
+        var coupon = await dbContext.Coupons
+                         .FirstOrDefaultAsync(x => x.ProductName == request.ProductName)
+                     ?? new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount" };
+
+        logger.LogInformation("Discount is retrieved for ProductName : {ProductName}, Amount : {Amount}",
+            coupon.ProductName, coupon.Amount);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
     }
 
     public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
@@ -19,7 +32,8 @@ public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
         return base.UpdateDiscount(request, context);
     }
 
-    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request,
+        ServerCallContext context)
     {
         return base.DeleteDiscount(request, context);
     }
