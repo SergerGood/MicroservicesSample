c90fe53eb4d07b2c610b3da2c92a81988fb298c5
sergergood <serzher@inbox.ru>
2025-03-06 22:38:04 +0300

    Integrate Discount gRPC service into StoreBasketCommandHandler

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Data;
+using Discount.Grpc;
 
 namespace Basket.API.Basket.StoreBasket;
 
@@ -15,14 +16,29 @@ internal class StoreBasketCommandValidator : AbstractValidator<StoreBasketComman
     }
 }
 
-internal class StoreBasketCommandHandler(IBasketRepository repository)
+internal class StoreBasketCommandHandler(
+    IBasketRepository repository,
+    DiscountProtoService.DiscountProtoServiceClient discountClient)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
         var cart = command.Cart;
+
+        await DeductDiscount(cart, cancellationToken);
         var basket = await repository.StoreBasketAsync(cart, cancellationToken);
 
         return new StoreBasketResult(basket.UserName);
+    }
+
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = item.ProductName };
+            var coupon = await discountClient.GetDiscountAsync(discountRequest, cancellationToken: cancellationToken);
+
+            item.Price -= coupon.Amount;
+        }
     }
 }
