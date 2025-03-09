23a8d506ae1ba7b9704a8a455242e8f1aad9a1be
sergergood <serzher@inbox.ru>
2025-03-09 17:43:41 +0300

    Add Dependency Injection setup for Ordering API, Application, and Infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database")
+                               ?? throw new IOException("Database connection string not found");
+        return services;
+    }
+}
