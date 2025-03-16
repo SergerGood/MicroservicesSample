7c86faaec97d8bc4a35539b52fef76d9a91c96a8
sergergood <serzher@inbox.ru>
2025-03-16 21:47:03 +0300

    Add CreateOrder

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,6 @@
+﻿using BuildingBlocks.CQRS;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
