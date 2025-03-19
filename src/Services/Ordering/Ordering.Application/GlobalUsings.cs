b7256fcd85d653512f8c3dccb8ed19241e7ca13b
sergergood <serzher@inbox.ru>
2025-03-19 21:58:38 +0300

    Add UpdateOrder command and handler with validation and exception handling

--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -1,6 +1,7 @@
 ﻿// Global using directives
 
 global using BuildingBlocks.CQRS;
+global using FluentValidation;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Domain.Models;
