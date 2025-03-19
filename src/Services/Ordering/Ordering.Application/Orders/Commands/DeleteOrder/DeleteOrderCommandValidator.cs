778bd38fb1e8a8307f7372ede5da9b61bb73d436
sergergood <serzher@inbox.ru>
2025-03-19 22:09:10 +0300

    Add DeleteOrder command, handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommandValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderCommandValidator() =>
+        RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderId is required");
+}
