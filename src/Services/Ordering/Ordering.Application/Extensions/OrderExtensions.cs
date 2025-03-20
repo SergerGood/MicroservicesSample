0111956f6161e1a75db62007018b49e9817fe309
sergergood <serzher@inbox.ru>
2025-03-20 22:24:23 +0300

    Refactor GetOrderByNameHandler to use extension method for DTO conversion

---
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Ordering.Application.Extensions;
+
+public static class OrderExtensions
+{
+    public static IEnumerable<OrderDto> ToOrderDto(this IEnumerable<Order> orders) =>
+        orders.Select(order => new OrderDto(
+            order.Id.Value,
+            order.CustomerId.Value,
+            order.OrderName.Value,
+            new AddressDto(
+                order.ShippingAddress.FirstName,
+                order.ShippingAddress.LastName,
+                order.ShippingAddress.EmailAddress,
+                order.ShippingAddress.AddressLine,
+                order.ShippingAddress.Country,
+                order.ShippingAddress.State,
+                order.ShippingAddress.ZipCode
+            ),
+            new AddressDto(
+                order.BillingAddress.FirstName,
+                order.BillingAddress.LastName,
+                order.BillingAddress.EmailAddress,
+                order.BillingAddress.AddressLine,
+                order.BillingAddress.Country,
+                order.BillingAddress.State,
+                order.BillingAddress.ZipCode
+            ),
+            new PaymentDto(
+                order.Payment.CardName,
+                order.Payment.CardNumber,
+                order.Payment.Expiration,
+                order.Payment.Cvv,
+                order.Payment.PaymentMethod),
+            order.Status,
+            order.OrderItems.Select(x =>
+                new OrderItemDto(x.OrderId.Value, x.ProductId.Value, x.Quantity, x.Price)).ToList()
+        ));
+}
--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByName/GetOrderByNameHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByName/GetOrderByNameHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Extensions;
 
 namespace Ordering.Application.Orders.Queries.GetOrderByName;
 
@@ -14,42 +15,6 @@ public class GetOrderByNameHandler(IApplicationDbContext dbContext)
             .OrderBy(x => x.OrderName)
             .ToListAsync(cancellationToken);
 
-        var ordersDtos = ProjectToOrderDto(orders);
-
-        return new GetOrderByNameResult(ordersDtos);
+        return new GetOrderByNameResult(orders.ToOrderDto());
     }
-
-    private IEnumerable<OrderDto> ProjectToOrderDto(List<Order> orders) =>
-        orders.Select(order => new OrderDto(
-            order.Id.Value,
-            order.CustomerId.Value,
-            order.OrderName.Value,
-            new AddressDto(
-                order.ShippingAddress.FirstName,
-                order.ShippingAddress.LastName,
-                order.ShippingAddress.EmailAddress,
-                order.ShippingAddress.AddressLine,
-                order.ShippingAddress.Country,
-                order.ShippingAddress.State,
-                order.ShippingAddress.ZipCode
-            ),
-            new AddressDto(
-                order.BillingAddress.FirstName,
-                order.BillingAddress.LastName,
-                order.BillingAddress.EmailAddress,
-                order.BillingAddress.AddressLine,
-                order.BillingAddress.Country,
-                order.BillingAddress.State,
-                order.BillingAddress.ZipCode
-            ),
-            new PaymentDto(
-                order.Payment.CardName,
-                order.Payment.CardNumber,
-                order.Payment.Expiration,
-                order.Payment.Cvv,
-                order.Payment.PaymentMethod),
-            order.Status,
-            order.OrderItems.Select(x =>
-                new OrderItemDto(x.OrderId.Value, x.ProductId.Value, x.Quantity, x.Price)).ToList()
-        ));
 }
