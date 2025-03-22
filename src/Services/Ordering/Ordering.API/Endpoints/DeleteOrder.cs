b2a183d5306aeeb2170f27901446ddb345bef0ad
sergergood <serzher@inbox.ru>
2025-03-22 20:05:03 +0300

    Add DeleteOrder and UpdateOrder endpoints

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,6 +1,27 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
 
-public class DeleteOrder
+namespace Ordering.API.Endpoints;
+
+public record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", Handle)
+            .WithName("DeleteOrder")
+            .Produces<DeleteOrderResponse>()
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status404NotFound)
+            .WithSummary("Delete order")
+            .WithDescription("Delete order");
+    }
+
+    private static async Task<IResult> Handle(Guid id, ISender sender)
+    {
+        var result = await sender.Send(new DeleteOrderCommand(id));
+        var response = result.Adapt<DeleteOrderResponse>();
+
+        return Results.Ok(response);
+    }
 }
--- src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
@@ -1,6 +1,29 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Orders.Commands.UpdateOrder;
 
-public class UpdateOrder
+namespace Ordering.API.Endpoints;
+
+public record UpdateOrderRequest(OrderDto Order);
+
+public record UpdateOrderResponse(bool IsSuccess);
+
+public class UpdateOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/order", Handle)
+            .WithName("UpdateOrder")
+            .Produces<UpdateOrderResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Updates order")
+            .WithDescription("Updates order");
+    }
 
+    private static async Task<IResult> Handle(UpdateOrderRequest request, ISender sender)
+    {
+        var command = request.Adapt<UpdateOrderCommand>();
+        var result = await sender.Send(command);
+        var response = result.Adapt<UpdateOrderResponse>();
+
+        return Results.Ok(response);
+    }
 }
