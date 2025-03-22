04b74538583e21a112c2bf2d758c3787a15d19c3
sergergood <serzher@inbox.ru>
2025-03-22 18:37:43 +0300

    Add endpoint classes

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,8 +1,20 @@
-﻿namespace Ordering.API;
+﻿using Carter;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services) => services;
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddCarter();
 
-    public static WebApplication UseApiServices(this WebApplication app) => app;
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        app.MapCarter();
+
+        return app;
+    }
 }
