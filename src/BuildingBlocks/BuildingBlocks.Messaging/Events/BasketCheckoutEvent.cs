3acba83b26864df22459599119e09344583f45db
sergergood <serzher@inbox.ru>
2025-03-25 22:20:45 +0300

    Add RabbitMQ configuration and messaging events

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,22 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+
+public record BasketCheckoutEvent : IntegrationEvent
+{
+    public required string UserName { get; init; }
+    public required Guid CustomerId { get; init; }
+    public required decimal TotalPrice { get; init; }
+
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string EmailAddress { get; init; }
+    public required string AddressLine { get; init; }
+    public required string Country { get; init; }
+    public required string State { get; init; }
+    public required string ZipCode { get; init; }
+
+    public required string CardName { get; init; }
+    public required string CardNumber { get; init; }
+    public required string Expiration { get; init; }
+    public required string Cvv { get; init; }
+    public required int PaymentMethod { get; init; }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+    {
+        services.AddMassTransit(configurator =>
+        {
+            configurator.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly is not null)
+            {
+                configurator.AddConsumers(assembly);
+            }
+
+            configurator.UsingRabbitMq((context, factoryConfigurator) =>
+            {
+                factoryConfigurator.Host(new Uri(configuration["RabbitMq:Host"]), hostConfigurator =>
+                {
+                    hostConfigurator.Username(configuration["RabbitMq:UserName"]);
+                    hostConfigurator.Password(configuration["RabbitMq:Password"]);
+                });
+                factoryConfigurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
