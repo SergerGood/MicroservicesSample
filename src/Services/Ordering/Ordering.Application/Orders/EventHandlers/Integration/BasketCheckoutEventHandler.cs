f6cf7dff50bfb6c09880dc5d265158cc0a19b3ee
sergergood <serzher@inbox.ru>
2025-03-26 21:15:01 +0300

    Add BasketCheckout event handling

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,39 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Orders.Commands.CreateOrder;
+using Ordering.Domain.Enums;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration;
+
+public class BasketCheckoutEventHandler(ISender sender, ILogger<BasketCheckoutEventHandler> logger)
+    : IConsumer<BasketCheckoutEvent>
+{
+    public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        logger.LogInformation("Integration event handled: {IntegrationEvent}", context.Message.GetType().Name);
+
+        var command = MapToCreateOrderCommand(context.Message);
+        await sender.Send(command);
+    }
+
+    private static CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutEvent message)
+    {
+        var addressDto = new AddressDto(message.FirstName, message.LastName, message.EmailAddress, message.AddressLine, message.Country, message.State, message.ZipCode);
+        var paymentDto = new PaymentDto(message.CardName, message.CardNumber, message.Expiration, message.Cvv, message.PaymentMethod);
+        var orderId = Guid.NewGuid();
+
+        var orderDto = new OrderDto(
+            orderId,
+            message.CustomerId,
+            message.UserName,
+            addressDto,
+            addressDto,
+            paymentDto,
+            OrderStatus.Pending,
+            []);
+
+        return new CreateOrderCommand(orderDto);
+    }
+}
