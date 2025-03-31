7f1afcfeae71aae0c1f7746b0dcab7d262164315
sergergood <serzher@inbox.ru>
2025-03-31 21:51:34 +0300

    Add Reverse Proxy routes and clusters configuration

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -4,10 +4,10 @@ using YarpApiGateway.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTransient<IProxyConfigProvider, ProxyConfigProvider>();
+// builder.Services.AddTransient<IProxyConfigProvider, ProxyConfigProvider>();
 
-builder.Services.AddReverseProxy();
-    // .LoadFromConfig(builder.Configuration.GetRequiredSection("ReverseProxy"));
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetRequiredSection("ReverseProxy"));
 
 var app = builder.Build();
 
