cd76bf95cf43819fe195db0b398e3196215cf90b
sergergood <serzher@inbox.ru>
2025-04-01 22:34:30 +0300

    Add WebApp project

---
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shopping.Web.Pages;
+
+public class IndexModel(ILogger<IndexModel> logger) : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ src/WebApps/Shopping.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shopping.Web.Pages;
+
+public class PrivacyModel(ILogger<PrivacyModel> logger) : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
