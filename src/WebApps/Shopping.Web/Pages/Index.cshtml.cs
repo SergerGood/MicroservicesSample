31db5da9ebc10566fcf1d455953c9628baa51ae3
sergergood <serzher@inbox.ru>
2025-04-06 17:36:46 +0300

    Add product listing home page

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shopping.Web.Models.Catalog;
 
 namespace Shopping.Web.Pages;
 
 public class IndexModel(ILogger<IndexModel> logger) : PageModel
 {
+    public IEnumerable<ProductModel> ProductList { get; set; } = [];
+
     public void OnGet()
     {
     }
