dc899c839383f88038bc7a01b0d257473fcc1ce5
sergergood <serzher@inbox.ru>
2025-04-09 21:20:19 +0300

    Add Checkout and Confirmation pages

---
+++ src/WebApps/Shopping.Web/Pages/Checkout.cshtml.cs
@@ -0,0 +1,36 @@
+﻿namespace Shopping.Web.Pages;
+
+public class CheckoutModel(IBasketService basketService, ILogger<CheckoutModel> logger) : PageModel
+{
+    [BindProperty]
+    public BasketCheckoutModel? Order { get; set; }
+
+    public ShoppingCartModel? Cart { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        Cart = await basketService.LoadUserBasket();
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostCheckOutAsync()
+    {
+        logger.LogInformation("Checkout button clicked");
+
+        Cart = await basketService.LoadUserBasket();
+
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        Order.CustomerId = new Guid("e8838071-72d3-4efa-910e-3303a9a9774f");
+        Order.UserName = Cart.UserName;
+        Order.TotalPrice = Cart.TotalPrice;
+
+        await basketService.CheckoutBasket(new CheckoutBasketRequest(Order));
+
+        return RedirectToPage("Confirmation", "OrderSubmitted");
+    }
+}
---
+++ src/WebApps/Shopping.Web/Pages/Confirmation.cshtml.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.Web.Pages;
+
+public class ConfirmationModel : PageModel
+{
+    public string? Message { get; set; }
+
+    public void OnGetContact() => Message = "Your email was sent";
+
+    public void OnGetOrderSubmitted() => Message = "Your order submitted successfully";
+}
